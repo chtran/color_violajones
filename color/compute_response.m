17e713cf8baffca43064dca6688b4d9b3871b1f2
Chau Tran <chau_tran@brown.edu>
2014-04-10 20:43:39 -0400

    WOrking on multi-scale

--- color/compute_response.m
+++ color/compute_response.m
@@ -1,25 +1,33 @@
-function response = compute_response(filter, integral_images)
+function responses = compute_response(x, y, w, h, filter, integral_images)
 % filter is a mx6 matrix where each row defines a rectangle
 % column 1 is the top left x value
 % column 2 is the top left y value
 % column 3 is the rectangle width
 % column 4 is the rectangle height
 % column 5 is the weight(coefficient)
-% column 6 is the channel number
-    response = 0;
-    for ii = 1:size(filter, 1)
-        x = filter(ii,1);
-        y = filter(ii,2);
-        width = filter(ii,3);
-        height = filter(ii,4);
-        weight = filter(ii,5);
-        channel = filter(ii,6);
+% column 6 is the channel number (1=R, 2=G, 3=B)
+    num_windows = size(x, 1);
+    responses = zeros(num_windows, 1);
+    for wd_id = 1:num_windows
+        wd_x = x(wd_id);
+        wd_y = y(wd_id);
+        wd_w = w(wd_id);
+        wd_h = h(wd_id);
+        
+        for ii = 1:size(filter, 1)
+            f_x = filter(ii,1);
+            f_y = filter(ii,2);
+            f_w = filter(ii,3);
+            f_h = filter(ii,4);
+            weight = filter(ii,5);
+            channel = filter(ii,6);
 
-        rect_response = integral_images(x+width, y+height, channel) ...
-            + integral_images(x, y, channel) ...
-            - integral_images(x+width, y, channel) ...
-            - integral_images(x, y+height, channel);
-        rect_response = weight*rect_response;
-        response = response + rect_response;
+            rect_response = integral_images(f_x+f_w, f_y+f_h, channel) ...
+                + integral_images(f_x, f_y, channel) ...
+                - integral_images(f_x+f_w, f_y, channel) ...
+                - integral_images(f_x, f_y+f_h, channel);
+            rect_response = weight*rect_response;
+            responses = responses + rect_response;
+        end
     end
 end
--- color/run.m
+++ color/run.m
@@ -2,10 +2,14 @@ filename = '../Images/1.jpg';
 integral_images = get_integral_images(imread(filename));
 
 
-% Mocking tree data
+% Example tree data
+cascade = struct();
+cascade.w = 20; % width of window
+cascade.h = 20; % height of window
+
 stage1 = struct();
 tree1 = struct();
-tree1.filter = [3 7 14 4 -1. 1; 3 9 14 2 2. 2];
+tree1.filter = [3 7 14 4 -1. 1; 3 9 14 2 2. 2]; % array of filters. See compute_response.m for description of filter
 tree1.threshold = 4.0141958743333817e-003;
 tree1.left_val = 0.0337941907346249;
 tree1.right_val = 0.8378106951713562;
@@ -16,7 +20,8 @@ tree2.threshold = 0.0151513395830989;
 tree2.left_val = 0.1514132022857666;
 tree2.right_val = 0.7488812208175659;
 stage1.trees = [tree1; tree2];
-stage1.threshold = 0.8226894140243530;
+stage1.stage_threshold = 0.8226894140243530;
 stages = [stage1];
+cascade.stages = stages;
 
-single_scale_detect(stages, integral_images)
+single_scale_detect(x, y, w, h, cascade, integral_images)
