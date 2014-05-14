f3d43317617152400448fb59bcd558db88de67c7
Chau Tran <chau_tran@brown.edu>
2014-05-14 03:37:16 -0400

    Only convert XML to mat on first run

--- demo_detection.m
+++ demo_detection.m
@@ -1,4 +1,7 @@
-ConvertHaarcasadeXMLOpenCV('HaarCascades/haarcascade_frontalface_alt.xml');
+if ~exist('HaarCascades/haarcascade_frontalface_alt.mat','file')
+    ConvertHaarcasadeXMLOpenCV('HaarCascades/haarcascade_frontalface_alt.xml');
+end
+
 I = imread('Images/3.jpg');
 FilenameHaarcasade = 'HaarCascades/haarcascade_frontalface_alt.mat';
 Objects=ObjectDetection(I,FilenameHaarcasade);
